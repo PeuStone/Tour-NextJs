dfcb1d3a0d8bbda54402ba2994435cef4ccbe75f
PeuStone <pedrohmotaaa@gmail.com>
2024-02-12 10:22:16 -0300

    Aprendendo sobre rotas dinâmicas com aninhamento utllizando o NextJs

--- pages/sobre.js
+++ pages/sobre.js
@@ -2,6 +2,12 @@ function SobrePage() {
     return (
         <div>
             Você está na página Sobre
+
+            <ul>
+                <li>
+                    <a href="/" >Ir para /Home</a>
+                </li>
+            </ul>
         </div>
     )
 }
