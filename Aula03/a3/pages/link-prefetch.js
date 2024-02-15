9998427a8997156faccd6c5aef2745a9374ad77b
PeuStone <pedrohmotaaa@gmail.com>
2024-02-15 10:36:07 -0300

    Estudos sobre Link Prefetch e Dynamic Import utilizando NextJs e a utilização do Typescript

---
+++ Aula03/a3/pages/link-prefetch.js
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+
+export default function LinkPrefetchPage() {
+  return (
+    <div>
+      <li>
+        <Link href="/" prefetch={false}>
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link href="/sobre" prefetch={false}>
+          Sobre
+        </Link>
+      </li>
+      <li>
+        <Link href="/link-prefetch">
+          Link Prefetch
+        </Link>
+      </li>
+    </div>
+  )
+}
